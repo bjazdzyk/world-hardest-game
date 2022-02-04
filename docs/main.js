f68d7731f95d3930976dbb32967417b8dc70112d
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-02-04 11:41:31 +0100

    enemies

--- docs/main.js
+++ docs/main.js
@@ -15,6 +15,9 @@ class level{
 		this.template = template
 		this.startPoint = startPoint
 		this.L = {}
+		this.enemies = {}
+		this.enemyIds = []
+
 		if(this.width > this.height){
 			this.renderSize = {x:screenWidth-100, y:(screenWidth-100)/this.width*this.height}
 		}else{
@@ -32,7 +35,6 @@ class level{
 	}
 	editSquare(x, y, type){
 		this.L[crd2str(x, y)] = type
-		console.log(crd2str(x, y))
 	}
 	fill(x, y, w, h, type){
 		for(let i=x; i<x+w; i++){
@@ -40,7 +42,42 @@ class level{
 				this.editSquare(i, j, type)
 			}
 		}
-		console.log(this.L)
+	}
+	addEnemy(id, path, ticksBetween, sizeScale = 0.2){
+		const radius = Math.min(this.cellSize.x, this.cellSize.y)*sizeScale
+		this.enemies[id] = {path: path, x:path[0][0], y:path[0][1], radius: radius}
+		this.enemyIds.push(id)
+
+		if(path.length > 1){
+			this.enemies[id].static = false
+			this.enemies[id].lastPoint = 0
+			this.enemies[id].destination = 1
+			const delta = {x: path[1][0]-path[0][0], y: path[1][1]-path[0][1]}
+			this.enemies[id].step = {}
+			this.enemies[id].step.x = delta.x/ticksBetween
+			this.enemies[id].step.y = delta.y/ticksBetween
+			this.enemies[id].ticksBetween = ticksBetween
+
+		}else{
+			this.enemies[id].static = true
+		}
+	}
+	update(){
+
+		for(let i=0; i<this.enemyIds.length; i++){
+			const enemy = this.enemies[this.enemyIds[i]]
+			const destinationDelta = Math.sqrt((enemy.path[enemy.destination][0]-enemy.x)*(enemy.path[enemy.destination][0]-enemy.x)+(enemy.path[enemy.destination][1]-enemy.y)*(enemy.path[enemy.destination][1]-enemy.y))
+			if(Math.abs(destinationDelta*this.cellSize.x) < 5){
+				this.enemies[this.enemyIds[i]].lastPoint = enemy.destination
+				this.enemies[this.enemyIds[i]].destination = (enemy.destination+1)%enemy.path.length
+				const delta = {x: enemy.path[this.enemies[this.enemyIds[i]].destination][0]-enemy.path[this.enemies[this.enemyIds[i]].lastPoint][0], y: enemy.path[this.enemies[this.enemyIds[i]].destination][1]-enemy.path[this.enemies[this.enemyIds[i]].lastPoint][1]}
+				this.enemies[this.enemyIds[i]].step = {}
+				this.enemies[this.enemyIds[i]].step.x = delta.x/enemy.ticksBetween
+				this.enemies[this.enemyIds[i]].step.y = delta.y/enemy.ticksBetween
+			}
+			this.enemies[this.enemyIds[i]].x += this.enemies[this.enemyIds[i]].step.x
+			this.enemies[this.enemyIds[i]].y += this.enemies[this.enemyIds[i]].step.y
+		}
 	}
 	render(){
 		ctx.fillStyle = "white"
@@ -48,10 +85,21 @@ class level{
 		for(let i=0; i<this.width; i++){
 			for(let j=0; j<this.height; j++){
 				if(this.L[crd2str(i, j)] === 1){
-					ctx.fillStyle = "skyblue"
+					ctx.fillStyle = "#70bfe0"
 					ctx.fillRect(this.renderAnchor.x+i*this.cellSize.x-1, this.renderAnchor.y+j*this.cellSize.y-1, this.cellSize.x+2, this.cellSize.y+2)
 				}
+				if(this.L[crd2str(i, j)] === 2){
+					ctx.fillStyle = "#8fffa0"//green
+					ctx.fillRect(this.renderAnchor.x+i*this.cellSize.x-0.5, this.renderAnchor.y+j*this.cellSize.y-0.5, this.cellSize.x+1, this.cellSize.y+1)
+				}
 			}
+		}
+		for(let i=0; i<this.enemyIds.length; i++){
+			const enemy = this.enemies[this.enemyIds[i]]
+			ctx.fillStyle = "red"
+			ctx.beginPath()
+			ctx.arc(this.renderAnchor.x+enemy.x*this.cellSize.x, this.renderAnchor.y+enemy.y*this.cellSize.y, enemy.radius, 0, 2*Math.PI)
+			ctx.fill()
 		}
 	}
 
@@ -64,13 +112,21 @@ levels[1].fill(3, 0, 1, 6, 1)
 levels[1].fill(4, 0, 7, 1, 1)
 levels[1].fill(5, 6, 7, 1, 1)
 levels[1].fill(12, 1, 1, 6, 1)
+levels[1].fill(0, 0, 3, 7, 2)
+levels[1].fill(13, 0, 3, 7, 2)
+levels[1].addEnemy("dot1", [[4.25, 1.5], [11.75, 1.5]], 50)
+levels[1].addEnemy("dot2", [[11.75, 2.5], [4.25, 2.5]], 50)
+levels[1].addEnemy("dot3", [[4.25, 3.5], [11.75, 3.5]], 50)
+levels[1].addEnemy("dot4", [[11.75, 4.5], [4.25, 4.5]], 50)
+levels[1].addEnemy("dot5", [[4.25, 5.5], [11.75, 5.5]], 50)
 
 
-ctx.fillStyle = "skyblue"
-ctx.fillRect(0, 0, 800, 600)
 
 const loop =()=>{
 	requestAnimationFrame(loop)
+	ctx.fillStyle = "#70bfe0"
+	ctx.fillRect(0, 0, 800, 600)
+	levels[1].update()
 	levels[1].render()
 }
 
